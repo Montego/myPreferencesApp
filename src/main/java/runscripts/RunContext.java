261281d6ee1f4cc39596f3e70d61bcdd416ba115
Montego <bar_d91@mail.ru>
2018-09-24 07:16:30 +0300

    separate Context build

---
+++ src/main/java/runscripts/RunContext.java
@@ -0,0 +1,24 @@
+package runscripts;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import service.impl.*;
+
+public class RunContext {
+
+    public RunContext(){
+        contextCome();
+    };
+    private static void contextCome() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        AuthorityService authorityService = context.getBean(AuthorityService.class);
+        CategoryService categoryService = context.getBean(CategoryService.class);
+        CountryService countryService = context.getBean(CountryService.class);
+        FriendsService friendsService = context.getBean(FriendsService.class);
+        GenreService genreService = context.getBean(GenreService.class);
+        ItemService itemService = context.getBean(ItemService.class);
+        RateService rateService = context.getBean(RateService.class);
+        ScoreService scoreService = context.getBean(ScoreService.class);
+        TypeService typeService = context.getBean(TypeService.class);
+        UserService userService = context.getBean(UserService.class);
+    }
+}
